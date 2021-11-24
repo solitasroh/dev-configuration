be1502ad757b67096b1bf72f36d80419298c58f9
solitasroh <78634894+solitasroh@users.noreply.github.com>
2021-11-24 17:08:15 +0900

    pc status id 오류 수정

--- src/application/PCContents.tsx
+++ src/application/PCContents.tsx
@@ -14,7 +14,9 @@ import PCCommandView from './pc/PCCommandView';
 const { Panel } = Collapse;
 
 export default function PCContents(): ReactElement {
+  const [index, setIndex] = useState(1);
   const [id, setId] = useState(1);
+  const [state, setState] = useState<boolean[]>([]);
   const [totalCount, setTotalCount] = useState(0);
   const [statusList, setStatusList] = useState<A2750PCStatus[]>([]);
 
@@ -23,6 +25,9 @@ export default function PCContents(): ReactElement {
     service.on('PC_STATUS_CHAGNED', (evt, rest) => {
       const buffer = rest as boolean[];
       const { length } = buffer.filter((v) => v);
+      
+      setState(buffer);
+
       setTotalCount(length);
     });
   }, []);
@@ -31,7 +36,7 @@ export default function PCContents(): ReactElement {
     requestType: 'A2750PCStatus',
     responseChannel: 'POLL-PC-STATUS',
     props: {
-      id,
+      id: index,
     },
   }, (evt, resp) => {
     const data = resp as A2750PCStatus[];
@@ -40,13 +45,23 @@ export default function PCContents(): ReactElement {
 
   const onChange = (page: number, pageSize: number) => {
     // 요청하는 ID만 달라지면 된다.
-    setId(page);
+    let validIdx = 0;
+    state.map((op, idx) => {
+      
+      if (op) {
+        validIdx +=1;
+        if (validIdx === page) {
+          setId(idx + 1);
+        }
+      }
+    })
+    setIndex(page);
   };
 
   return (
     <Space direction="vertical">
       <Pagination
-        defaultCurrent={id}
+        defaultCurrent={index}
         total={totalCount}
         size="small"
         defaultPageSize={1}
@@ -56,14 +71,14 @@ export default function PCContents(): ReactElement {
       <Space align="start" direction="horizontal">
         <Collapse defaultActiveKey={['1']} bordered={false} ghost>
           <Panel header="Status" key="1">
-            {statusList[id - 1] == null ? (
+            {statusList[index - 1] == null ? (
               <Empty />
             ) : (
               <Space align="start">
-                <DefaultView id={id} status={statusList[id - 1]} />
-                <DIStatusView id={id} status={statusList[id - 1]} />
-                <DOStatusView id={id} status={statusList[id - 1]} />
-                <ProtectionView id={id} status={statusList[id - 1]} />
+                <DefaultView id={id} status={statusList[index - 1]} />
+                <DIStatusView id={id} status={statusList[index - 1]} />
+                <DOStatusView id={id} status={statusList[index - 1]} />
+                <ProtectionView id={id} status={statusList[index - 1]} />
               </Space>
             )}
           </Panel>
--- src/application/pc/PCStatusView.tsx
+++ src/application/pc/PCStatusView.tsx
@@ -32,27 +32,27 @@ export const DefaultView: FC<Props> = ({ id, status }) => (
       <Space size="small" direction="vertical">
         <Space size="small">
           <Label>Starting block</Label>
-          <Status on={status.startingBlock} />
+          <Status on={status.startingBlock ?? false} />
         </Space>
         <Space size="small">
           <Label>Motor op state</Label>
-          <Status on={status.motorOperationState} />
+          <Status on={status.motorOperationState ?? false} />
         </Space>
         <Space size="small">
           <Label>Remote mode</Label>
-          <Status on={status.remoteStatus} />
+          <Status on={status.remoteStatus ?? false} />
         </Space>
         <Space size="small">
           <Label>abnormal</Label>
-          <Status on={status.abnormalState} />
+          <Status on={status.abnormalState ?? false} />
         </Space>
         <Space size="small">
           <Label>alarm</Label>
-          <Status on={status.alarmState} />
+          <Status on={status.alarmState ?? false} />
         </Space>
         <Space size="small">
           <Label>fault</Label>
-          <Status on={status.faultState} />
+          <Status on={status.faultState ?? false} />
         </Space>
       </Space>
     </Card>
@@ -64,43 +64,43 @@ export const DIStatusView: FC<Props> = ({ id, status }) => (
       <Space size="small" direction="vertical">
         <Space size="small">
           <Label>DI 1</Label>
-          <Status on={status.di1} />
+          <Status on={status.di1 ?? false} />
         </Space>
         <Space size="small">
           <Label>DI 2</Label>
-          <Status on={status.di2} />
+          <Status on={status.di2 ?? false} />
         </Space>
         <Space size="small">
           <Label>DI 3</Label>
-          <Status on={status.di3} />
+          <Status on={status.di3 ?? false} />
         </Space>
         <Space size="small">
           <Label>DI 4</Label>
-          <Status on={status.di4} />
+          <Status on={status.di4 ?? false} />
         </Space>
         <Space size="small">
           <Label>DI 5</Label>
-          <Status on={status.di5} />
+          <Status on={status.di5 ?? false} />
         </Space>
         <Space size="small">
           <Label>DI 6</Label>
-          <Status on={status.di6} />
+          <Status on={status.di6 ?? false} />
         </Space>
         <Space size="small">
           <Label>DI 7</Label>
-          <Status on={status.di7} />
+          <Status on={status.di7 ?? false} />
         </Space>
         <Space size="small">
           <Label>DI 8</Label>
-          <Status on={status.di8} />
+          <Status on={status.di8 ?? false} />
         </Space>
         <Space size="small">
           <Label>DI 9</Label>
-          <Status on={status.di9} />
+          <Status on={status.di9 ?? false} />
         </Space>
         <Space size="small">
           <Label>DI 10</Label>
-          <Status on={status.di10} />
+          <Status on={status.di10 ?? false} />
         </Space>
       </Space>
     </Card>
@@ -113,19 +113,19 @@ export const DOStatusView: FC<Props> = ({ id, status }) => (
       <Space size="small" direction="vertical">
         <Space size="small">
           <Label>DO 1</Label>
-          <Status on={status.do1} />
+          <Status on={status.do1 ?? false} />
         </Space>
         <Space size="small">
           <Label>DO 2</Label>
-          <Status on={status.do2} />
+          <Status on={status.do2 ?? false} />
         </Space>
         <Space size="small">
           <Label>DO 3</Label>
-          <Status on={status.do3} />
+          <Status on={status.do3 ?? false} />
         </Space>
         <Space size="small">
           <Label>DO 4</Label>
-          <Status on={status.do4} />
+          <Status on={status.do4 ?? false} />
         </Space>
       </Space>
     </Card>
@@ -138,50 +138,49 @@ export const ProtectionView: FC<Props> = ({ id, status }) => (
       <Space size="small" direction="vertical">
         <Space size="small">
           <Label>THR</Label>
-          <Status on={status.thr} />
+          <Status on={status.thr ?? false} />
         </Space>
         <Space size="small">
           <Label>OCR</Label>
-          <Status on={status.ocr} />
+          <Status on={status.ocr ?? false} />
         </Space>
         <Space size="small">
           <Label>POCR</Label>
-          <Status on={status.pocr} />
+          <Status on={status.pocr ?? false} />
         </Space>
         <Space size="small">
           <Label>PSR</Label>
-          <Status on={status.psr} />
+          <Status on={status.psr ?? false} />
         </Space>
         <Space size="small">
           <Label>UBCR</Label>
-          <Status on={status.ubcr} />
+          <Status on={status.ubcr ?? false} />
         </Space>
         <Space size="small">
           <Label>JAM</Label>
-          <Status on={status.jam} />
+          <Status on={status.jam ?? false} />
         </Space>
         <Space size="small">
           <Label>LSR</Label>
-          <Status on={status.lsr} />
+          <Status on={status.lsr ?? false} />
         </Space>
         <Space size="small">
           <Label>GR(ZCT)</Label>
-          <Status on={status.grzct} />
+          <Status on={status.grzct ?? false} />
         </Space>
         <Space size="small">
           <Label>GR(CT)</Label>
-          <Status on={status.grct} />
+          <Status on={status.grct ?? false} />
         </Space>
         <Space size="small">
           <Label>UCR</Label>
-          <Status on={status.ucr} />
+          <Status on={status.ucr ?? false} />
         </Space>
         <Space size="small">
           <Label>MCCB TRIP</Label>
-          <Status on={status.mccbTrip} />
+          <Status on={status.mccbTrip ?? false} />
         </Space>
       </Space>
     </Card>
   </List.Item>
 );
-
