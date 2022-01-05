52c4e5f99d975754880f718c480d8b254f87a0db
nhkim1222 <nhkim@rootech.com>
2022-01-05 16:45:02 +0900

    Update RegisterLMManageSetup.ts
    LM Management setup read하도록 수정

--- src/main/modbus.a2700m/lmh/RegisterLMManageSetup.ts
+++ src/main/modbus.a2700m/lmh/RegisterLMManageSetup.ts
@@ -4,6 +4,7 @@ import LMManagementSetup from '@src/Data/LMHManageSetup';
 import { map, Observable, throwError } from 'rxjs';
 import ModbusService from '../../ModbusService';
 import RegisterBase from '../RegisterBase';
+import RegisterProps from '../RegisterProps';
 
 export default class RegisterLMManageSetup extends RegisterBase {
   setter = (data: any): void => {
@@ -23,8 +24,10 @@ export default class RegisterLMManageSetup extends RegisterBase {
     
   };
 
-  getter = (): Observable<A2700Data | A2700Data[]> =>
-    ModbusService.read<number[]>(51040, 5).pipe(
+  getter = (_params?: RegisterProps): Observable<A2700Data | A2700Data[]> => 
+  {    
+    ModbusService.write(65535, [0xffff]).subscribe();
+    const mode =  ModbusService.read<number[]>(51040, 5).pipe(
       map((data) => {
         const result = new LMManagementSetup();
 
@@ -33,8 +36,11 @@ export default class RegisterLMManageSetup extends RegisterBase {
         ] = data as number[];
 
         result.managementMode = managementMode;
-
         return result;
       }),
-    );
+    );    
+    ModbusService.write(65535, [0]).subscribe();
+
+    return mode;
+  }
 }
