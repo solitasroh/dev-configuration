817b23030f5b7ad4bdbeb94860e4b24b16f54bc3
solitasroh <78634894+solitasroh@users.noreply.github.com>
2022-02-07 10:12:05 +0900

    register element 데이터 구조 등록

---
+++ src/Data/RegisterElement.ts
@@ -0,0 +1,13 @@
+
+export default class RegisterElement {
+    
+    WrappedAddress: number;
+    
+    Length : number;
+
+    Page : number;
+
+    Address: number;
+
+    type: number; // 0: register 1: coil
+}
