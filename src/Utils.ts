a52e40527695052c392cd573fd556e116b142ef1
nhkim1222 <nhkim@rootech.com>
2022-02-17 13:45:15 +0900

    wrapperd map 파일전송 수정

--- src/Utils.ts
+++ src/Utils.ts
@@ -1,3 +1,4 @@
+/* eslint-disable no-bitwise */
 /**
  * Returns an array with arrays of the given size.
  *
@@ -16,12 +17,53 @@ function chunkArray(myArray: any, chunkSize: number): Array<any> {
 
   return results;
 }
+const crcTable = [256];
 
-export function calculateCRC(buffer: Buffer, length: number) : number {
-  let crc = 0;
-  crc = 1;
-
-  return crc;
+export function calculateCRC(buffer: Uint8Array, length: number): number {
+  let uchCRCHi = 0xff;
+  let uchCRCLo = 0xff;
+  let crcIndex;
+  let index;
+  
+  console.log(`buffer size = ${buffer.byteLength}`);
+  let count = 0;
+  for (index = 0; index < length; index += 1) {
+    if (buffer[index] !== undefined) {
+      count += 1;
+  //    console.log(buffer[index]);
+    }
+    
+    crcIndex = uchCRCHi ^ buffer[index];
+    uchCRCHi = uchCRCLo ^ (crcTable[crcIndex] & 0xff);
+    uchCRCLo = (crcTable[crcIndex] >> 8) & 0xff;
+  }
+  console.log("crc count ", count);
+  return (uchCRCHi << 8) | uchCRCLo;
 }
+
+function initCrc(): void {
+  let c;
+
+  const genCrc = (data: number, polynomial: number): number => {
+    let crc;
+    let d = data;
+    
+    for (crc = 0, c = 0; c < 8; c += 1) {
+      if (((d ^ crc) & 1) !== 0) {
+        crc = (crc >> 1) ^ polynomial;
+      } else {
+        crc >>= 1;
+      }
+      d >>= 1;
+    }
+    return crc & 0xffff;
+  };
+
+  for (let i = 0; i < 256; i += 1) {
+    crcTable[i] = genCrc(i, 0xa001);
+  }
+}
+
+initCrc();
 
 export default chunkArray;
--- src/main/ipc/ChannelSendToDevice.ts
+++ src/main/ipc/ChannelSendToDevice.ts
@@ -9,6 +9,7 @@ import A2700Register from '../modbus.a2700m/A2700M.Register';
 import ModbusService from '../ModbusService';
 import { IpcRequest } from './IPCRequest';
 import { async } from 'rxjs';
+import { calculateCRC } from '@src/Utils';
 
 const sleep = (ms: number) => new Promise((resolve) => setTimeout(resolve, ms));
 
@@ -41,67 +42,71 @@ export class ChannelSendToDevice
     request: ChannelSendToDeviceProps,
   ): Promise<void> {
     const { fileType, elements } = request;
-    const elementToStr = elements.map(e => `#ELEMENT ${e.wrappedAddress} ${e.length} ${e.page} ${e.address}\r\n`)
+    const elementToStr = elements.map(
+      (e) =>
+        `#ELEMENT ${e.wrappedAddress} ${e.length} ${e.page} ${e.address}\r\n`,
+    );
     elementToStr.push('#END\r\n');
     const data = Buffer.from(elementToStr.join(''), 'utf-8');
-    const crc = calculateCrc(data, data.byteLength);
+
+    console.log(`len = ${data.length} bLen = ${data.byteLength}`);
+    console.log(data);
+
     const service = ModbusService.GetClient();
 
     const type = fileType === 1 ? 2 : 1;
+    console.log(data.byteOffset, data.length>>1);
+    const buffer = new Uint16Array(data.buffer, data.byteOffset, data.length>>1);
 
-    const buffer = new Uint16Array(data.buffer, data.byteOffset, data.length);
     const result = Array.from(buffer);
-    console.log(`bLen = ${buffer.byteLength} u16Len = ${result.length}`);
+    
+    const crc = calculateCRC(data, data.length);
+    
     try {
       service.writeRegister(65534, 65535);
-      
+
       const authority = await this.getAuthority();
-
+      const length = [ data.length >> 16, data.length & 0xFFFF];
       if (authority) {
-        const state = await this.getWrappedRegisterWriteState();
-        if (state === 0) {
-          
-          service.writeRegister(40202, type);
-          // crc, filesize,
+        await service.writeRegister(40202, type);
+        await service.writeRegister(40203, crc);
+        await service.writeRegisters(40204, length);
+        await service.writeRegister(40201, 1);
 
-          service.writeRegister(40201, 1); //command
-        }
-  
         const success = await this.retryReadState(0, 0);
         if (success) {
           const wLen = result.length < 120 ? result.length : 120;
           console.log(`remaining ${result.length} readLength ${wLen}`);
-  
+
           await this.retryWriteFileContents(result, 0, wLen, result.length);
-  
+
           event.sender.send(request.responseChannel, true);
         } else {
           console.log('read state failed');
           event.sender.send(request.responseChannel, false);
         }
       }
-     
     } catch (error) {
       event.sender.send(request.responseChannel, false);
     }
   }
 
-  getAuthority = async () : Promise<boolean> => {
+  getAuthority = async (): Promise<boolean> => {
     const service = ModbusService.GetClient();
     await service.writeRegister(40199, 0x8000);
 
     const result = await service.readHoldingRegisters(40200, 1);
     return result.data[0] === 1;
-  }
+  };
 
   getWrappedRegisterWriteState = async (): Promise<number> => {
     const service = ModbusService.GetClient();
-    const readRegisters = await service.readHoldingRegisters(40203, 1);
+    const readRegisters = await service.readHoldingRegisters(40206, 1);
     return readRegisters.data[0];
   };
 
   retryReadState = async (state: number, count: number): Promise<boolean> => {
-    if (state === 2) {
+    if (state === 1) {
       return true;
     }
 
@@ -139,12 +144,15 @@ export class ChannelSendToDevice
     console.log(`remaining ${remaining} readLength ${readLength}`);
 
     try {
-      await service.writeRegisters(40204, writeBuffer);
-      await service.writeRegister(40329, wlen * 2);
-
-      if (remaining <= 0) {
-        await service.writeRegister(40329, 0);
-        return true;
+      await service.writeRegisters(40208, writeBuffer);
+      const resultOprationStatus = await service.readHoldingRegisters(40206, 1);
+      const resultError = await service.readHoldingRegisters(40207, 1);
+      console.log(`state = ${resultOprationStatus.data[0]} error= ${resultError.data[0]}`);
+      if (resultOprationStatus.data[0] === 4) {
+        if (resultError.data[0] === 1) console.log(`Write error`);
+        else if (resultError.data[0] === 3) console.log(`timeout error`);
+        else if (resultError.data[0] === 4) console.log(`CRC error`);
+        else console.log("no error");
       }
       return await this.retryWriteFileContents(
         data,
