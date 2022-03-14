0e310537937cc92f442faa72f0b96ee2f8a51334
solitasroh <78634894+solitasroh@users.noreply.github.com>
2022-03-14 17:05:15 +0900

    * module type 설정 register 구현 완료

--- src/Data/IOHLogicalTypeSetup.ts
+++ src/Data/IOHLogicalTypeSetup.ts
@@ -1,8 +1,18 @@
 import A2700Data from './A2700Data';
 import A2700DataType from './A2700DataType';
+export interface LogicModuleType {
+  moduleType: number;
+  exist: boolean;
+}
 
 export default class IOHLogicalTypeSetup implements A2700Data {
   type: A2700DataType = A2700DataType.LMHLogicSetup;
-  moduleType: number;
-  exist: boolean;
+  moduleTypes: LogicModuleType[];
+
+  constructor() {
+    this.moduleTypes = [];
+    for (let i = 0; i < 15; i += 1) {
+      this.moduleTypes.push({ moduleType: 0, exist: false });
+    }
+  }
 }
--- src/main/modbus.a2700m/ioh/RegisterIOHLogicalTypeSetup.ts
+++ src/main/modbus.a2700m/ioh/RegisterIOHLogicalTypeSetup.ts
@@ -6,52 +6,35 @@ import ModbusService from '@src/main/ModbusService';
 import IOHLogicalTypeSetup from '@src/Data/IOHLogicalTypeSetup';
 
 export default class RegisterIOHLogicalTypeSetup extends RegisterBase {
-  private idSetAddress = 61909;
-  private accessAddress = 61910;
-  private dataAddress = 61911;
+  private mapPage = 0xffff;
+  private dataAddress = 52004;
 
   getter(_params?: RegisterProps): Observable<A2700Data | A2700Data[]> {
     console.log('ioh logical io setup access id : ', _params.id);
-    const registerAccessID = ModbusService.write(this.idSetAddress, [
-      _params.id,
-    ]);
-    const registerAccess = ModbusService.read<number[]>(this.accessAddress, 1);
-    const registerData = ModbusService.read<number[]>(this.dataAddress, 1);
+    const pageChange = ModbusService.write(65535, [this.mapPage]);
 
-    return forkJoin([registerAccessID, registerAccess, registerData]).pipe(
+    const data = ModbusService.read<number[]>(this.dataAddress, 15);
+    return forkJoin([pageChange, data]).pipe(
       map((resp) => {
-        const [_, acc, buffer] = resp;
-        const setup = new IOHLogicalTypeSetup();
-
-        if (acc[0] === 0x8000) {
-          const type = buffer[0] >> 8;
-          const exist = buffer[0] & 0xff;
-          setup.type = type;
-          setup.exist = exist === 1;
+        const [_, d] = resp;
+        const moduleTypeSetup = new IOHLogicalTypeSetup();
+        for (let i = 0; i < 15; i += 1) {
+          moduleTypeSetup.moduleTypes[i].moduleType = d[i];
         }
-        return setup;
+        return moduleTypeSetup;
       }),
     );
   }
 
   async setter(_data: any): Promise<void> {
-    const unlock = await this.unlockSetup();
-    const registerAccessID = ModbusService.write(this.idSetAddress, [_data.id]);
-    const registerAccess = ModbusService.read<number[]>(this.accessAddress, 1);
-
     const buffer = [];
+    for (let i = 0; i < 15; i += 1) {
+      const type = _data.moduleTypes[i].moduleType;
+      buffer.push(type);
+    }
+    const pageChange = ModbusService.write(65535, [this.mapPage]);
+    var data = ModbusService.write(this.dataAddress, buffer);
 
-    const type = _data.type;
-    const exist = _data.exist ? 1 : 0;
-
-    buffer.push(type | (exist << 8));
-
-    const ob1 = ModbusService.write(this.dataAddress, buffer);
-    const ob2 = ModbusService.write(this.accessAddress, [1]);
-
-    forkJoin([unlock, registerAccessID, registerAccess, ob1, ob2]).subscribe({
-      next: () => {},
-      complete: () => {},
-    });
+    forkJoin([pageChange, data]).subscribe();
   }
 }
